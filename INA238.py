35b4c48370ed4b9d0067fbfdf2aa8b6d839a9f42
Luca <102960244+Luca-Timo@users.noreply.github.com>
2024-07-01 09:48:18 +0200

    Update INA238.py

--- INA238.py
+++ INA238.py
@@ -4,142 +4,144 @@ class DeviceRangeError(Exception):
     pass
 
 class INA238:
-    # Register addresses for INA238 (as per datasheet)
-    CONFIG_REGISTER = 0x00
-    SHUNT_VOLTAGE_REGISTER = 0x01
-    BUS_VOLTAGE_REGISTER = 0x02
-    POWER_REGISTER = 0x03
-    CURRENT_REGISTER = 0x04
-    CALIBRATION_REGISTER = 0x05
-    DIAG_ALRT_REGISTER = 0x06
+    # INA238 Register Addresses
+    REG_CONFIG = 0x00
+    REG_ADC_CONFIG = 0x01
+    REG_SHUNT_CAL = 0x02
+    REG_VSHUNT = 0x04
+    REG_VBUS = 0x05
+    REG_DIETEMP = 0x06
+    REG_CURRENT = 0x07
+    REG_POWER = 0x08
+    REG_DIAG_ALRT = 0x0B
+    REG_SOVL = 0x0C
+    REG_SUVL = 0x0D
+    REG_BOVL = 0x0E
+    REG_BUVL = 0x0F
+    REG_TEMP_LIMIT = 0x10
+    REG_PWR_LIMIT = 0x11
+    REG_MANUFACTURER_ID = 0x3E
+    REG_DEVICE_ID = 0x3F
 
-    # Configuration constants (from datasheet or application requirements)
-    RANGE_16V = 0x0000  # Example constant, INA238 supports up to 85V
-    RANGE_32V = 0x0001  # Example constant, INA238 supports up to 85V
-
-    GAIN_1_40MV = 0x0000
-    GAIN_2_80MV = 0x0001
-    GAIN_4_160MV = 0x0002
-    GAIN_8_320MV = 0x0003
-
-    ADC_9BIT = 0x0000
-    ADC_10BIT = 0x0001
-    ADC_11BIT = 0x0002
-    ADC_12BIT = 0x0003
-    ADC_2SAMP = 0x0009
-    ADC_4SAMP = 0x000A
-    ADC_8SAMP = 0x000B
-    ADC_16SAMP = 0x000C
-    ADC_32SAMP = 0x000D
-    ADC_64SAMP = 0x000E
-    ADC_128SAMP = 0x000F
-
-    def __init__(self, i2c_bus=1, address=0x40):
-        self.bus = smbus.SMBus(i2c_bus)
-        self.address = address
+    def __init__(self, bus_num=1, addr=0x40):
+        self.bus = smbus.SMBus(bus_num)
+        self.addr = addr
+        self.reset()
         self.configure()
 
-    def configure(self, voltage_range=RANGE_32V, gain=GAIN_1_40MV, bus_adc=ADC_12BIT, shunt_adc=ADC_12BIT):
-        config = 0x0000
-        config |= voltage_range
-        config |= (gain << 1)
-        config |= (bus_adc << 3)
-        config |= (shunt_adc << 7)
+    def write_register(self, reg, value):
+        self.bus.write_word_data(self.addr, reg, value)
 
-        self.bus.write_word_data(self.address, INA238.CONFIG_REGISTER, config)
-        self.bus.write_word_data(self.address, INA238.CALIBRATION_REGISTER, INA238.CALIBRATION_VALUE)
+    def read_register(self, reg):
+        return self.bus.read_word_data(self.addr, reg)
 
-    def read_shunt_voltage(self):
-        raw_value = self.bus.read_word_data(self.address, INA238.SHUNT_VOLTAGE_REGISTER)
-        shunt_voltage = self._convert_raw_shunt_voltage(raw_value)
-        return shunt_voltage
+    def configure(self, voltage_range=0x0000, adc_range=0x0000, bus_adc=0x0780, shunt_adc=0x0078):
+        config = voltage_range | adc_range
+        adc_config = bus_adc | shunt_adc
+        self.write_register(self.REG_CONFIG, config)
+        self.write_register(self.REG_ADC_CONFIG, adc_config)
 
-    def read_bus_voltage(self):
-        raw_value = self.bus.read_word_data(self.address, INA238.BUS_VOLTAGE_REGISTER)
-        bus_voltage = self._convert_raw_bus_voltage(raw_value)
-        return bus_voltage
+    def calibrate(self, shunt_resistance):
+        calibration_value = int(8192 / (shunt_resistance * 0.00512))
+        self.write_register(self.REG_SHUNT_CAL, calibration_value)
 
-    def read_power(self):
-        raw_value = self.bus.read_word_data(self.address, INA238.POWER_REGISTER)
-        power = self._convert_raw_power(raw_value)
-        return power
-
-    def read_current(self):
-        raw_value = self.bus.read_word_data(self.address, INA238.CURRENT_REGISTER)
-        current = self._convert_raw_current(raw_value)
-        return current
-
-    def _convert_raw_shunt_voltage(self, raw_value):
-        shunt_voltage = raw_value * 1.25e-6  # Convert to volts (example, based on 1.25 µV LSB)
-        return shunt_voltage
-
-    def _convert_raw_bus_voltage(self, raw_value):
-        bus_voltage = raw_value * 3.125e-3  # Convert to volts (example, based on 3.125 mV LSB)
-        return bus_voltage
-
-    def _convert_raw_power(self, raw_value):
-        power = raw_value * 25e-6  # Convert to watts (example, based on 25 µW LSB)
-        return power
-
-    def _convert_raw_current(self, raw_value):
-        current = raw_value * 1e-6  # Convert to amps (example, based on 1 µA LSB)
-        return current
-
-    # INA219 compatible methods
     def voltage(self):
-        return self.read_bus_voltage()
-
-    def supply_voltage(self):
-        return self.read_bus_voltage() + self.read_shunt_voltage()
-
-    def current(self):
-        current = self.read_current()
-        if self.current_overflow():
-            raise DeviceRangeError("Current overflow")
-        return current * 1000  # Convert to mA
-
-    def power(self):
-        power = self.read_power()
-        if self.current_overflow():
-            raise DeviceRangeError("Current overflow")
-        return power * 1000  # Convert to mW
+        return self.read_register(self.REG_VBUS) * 0.003125
 
     def shunt_voltage(self):
-        shunt_voltage = self.read_shunt_voltage()
+        raw_shunt_voltage = self.read_register(self.REG_VSHUNT)
+        return raw_shunt_voltage * 0.00125 if raw_shunt_voltage & 0x8000 else raw_shunt_voltage * 0.005
+
+    def current(self):
+        current = self.read_register(self.REG_CURRENT) * 0.001
         if self.current_overflow():
             raise DeviceRangeError("Current overflow")
-        return shunt_voltage * 1000  # Convert to mV
+        return current
+
+    def power(self):
+        power = self.read_register(self.REG_POWER) * 0.001
+        if self.current_overflow():
+            raise DeviceRangeError("Current overflow")
+        return power
+
+    def supply_voltage(self):
+        return self.voltage() + self.shunt_voltage()
 
     def current_overflow(self):
-        diag_alrt = self.bus.read_word_data(self.address, INA238.DIAG_ALRT_REGISTER)
-        return bool(diag_alrt & 0x0001)  # Assuming overflow is indicated by the first bit
+        diag_alrt = self.read_register(self.REG_DIAG_ALRT)
+        return bool(diag_alrt & 0x0020)
 
     def sleep(self):
-        config = self.bus.read_word_data(self.address, INA238.CONFIG_REGISTER)
-        config &= 0xFFF8  # Set MODE bits to 000 (shutdown)
-        self.bus.write_word_data(self.address, INA238.CONFIG_REGISTER, config)
+        config = self.read_register(self.REG_CONFIG)
+        config &= ~0x8000
+        self.write_register(self.REG_CONFIG, config)
 
     def wake(self):
-        config = self.bus.read_word_data(self.address, INA238.CONFIG_REGISTER)
-        config |= 0x0007  # Set MODE bits to continuous mode
-        self.bus.write_word_data(self.address, INA238.CONFIG_REGISTER, config)
+        config = self.read_register(self.REG_CONFIG)
+        config |= 0x8000
+        self.write_register(self.REG_CONFIG, config)
 
     def reset(self):
-        self.bus.write_word_data(self.address, INA238.CONFIG_REGISTER, 0x8000)  # Set RST bit to reset
+        self.write_register(self.REG_CONFIG, 0x8000)
+        time.sleep(0.1)  # Delay to ensure reset
 
     def is_conversion_ready(self):
-        diag_alrt = self.bus.read_word_data(self.address, INA238.DIAG_ALRT_REGISTER)
-        return bool(diag_alrt & 0x0008)  # Assuming CNVR bit is the 4th bit
+        diag_alrt = self.read_register(self.REG_DIAG_ALRT)
+        return bool(diag_alrt & 0x0002)
+
+    def get_temperature(self):
+        raw_temp = self.read_register(self.REG_DIETEMP)
+        return (raw_temp >> 4) * 0.125  # Conversion factor: 125 m°C/LSB
+
+    def set_overvoltage_threshold(self, threshold):
+        self.write_register(self.REG_BOVL, int(threshold / 0.003125))  # Conversion factor: 3.125 mV/LSB
+
+    def set_undervoltage_threshold(self, threshold):
+        self.write_register(self.REG_BUVL, int(threshold / 0.003125))  # Conversion factor: 3.125 mV/LSB
+
+    def set_power_limit(self, limit):
+        self.write_register(self.REG_PWR_LIMIT, int(limit / (256 * 0.001)))  # Conversion factor: 256 * Power LSB
+
+    def read_config(self):
+        return self.read_register(self.REG_CONFIG)
+
+    def read_adc_config(self):
+        return self.read_register(self.REG_ADC_CONFIG)
+
+    def read_shunt_cal(self):
+        return self.read_register(self.REG_SHUNT_CAL)
+
+    def read_vshunt(self):
+        return self.read_register(self.REG_VSHUNT)
+
+    def read_vbus(self):
+        return self.read_register(self.REG_VBUS)
+
+    def read_dietemp(self):
+        return self.read_register(self.REG_DIETEMP)
+
+    def read_diag_alrt(self):
+        return self.read_register(self.REG_DIAG_ALRT)
+
+    def set_shunt_overvoltage_limit(self, limit):
+        self.write_register(self.REG_SOVL, int(limit / 0.00125))  # Conversion factor: 1.25 mV/LSB
+
+    def set_shunt_undervoltage_limit(self, limit):
+        self.write_register(self.REG_SUVL, int(limit / 0.00125))  # Conversion factor: 1.25 mV/LSB
+
+    def set_temperature_limit(self, limit):
+        self.write_register(self.REG_TEMP_LIMIT, int(limit / 0.125))  # Conversion factor: 125 m°C/LSB
 
 # Usage example
 # sensor = INA238()
-# sensor.configure(voltage_range=INA238.RANGE_32V, gain=INA238.GAIN_1_40MV, bus_adc=INA238.ADC_12BIT, shunt_adc=INA238.ADC_12BIT)
+# sensor.configure(voltage_range=INA238.RANGE_32V, adc_range=INA238.ADC_RANGE, bus_adc=INA238.ADC_12BIT, shunt_adc=INA238.ADC_12BIT)
 # print("Bus Voltage: {:.2f} V".format(sensor.voltage()))
 # print("Supply Voltage: {:.2f} V".format(sensor.supply_voltage()))
 # print("Shunt Voltage: {:.2f} mV".format(sensor.shunt_voltage()))
 # print("Current: {:.2f} mA".format(sensor.current()))
 # print("Power: {:.2f} mW".format(sensor.power()))
 # print("Current Overflow: {}".format(sensor.current_overflow()))
+# print("Temperature: {:.2f} °C".format(sensor.get_temperature()))
 # sensor.sleep()
 # sensor.wake()
 # sensor.reset()
